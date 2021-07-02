c7d952fa45b145f1fe40cb81c9c0ef2b4948ad32
veerenCFE <38349480+veerenCFE@users.noreply.github.com>
2021-07-01 22:21:40 -0700

    Create UpsertVertex.js
    This Stored Proc (Cosmos SP) will help to create or modify the Vertex (if the Partition Key is same)

---
+++ UpsertVertex.js
@@ -0,0 +1,19 @@
+function spupsertvertex (doc) {
+    var context = getContext();
+    var collection = context.getCollection();
+    var collectionLink = collection.getSelfLink();
+    var response = context.getResponse();
+    
+ if (!doc) {  throw new Error('Missing required parameter.');  }
+    var documents = typeof doc === "string" ? JSON.parse(doc) : doc;
+    
+     documents.forEach(function (document) {
+           var err = !collection.upsertDocument(collectionLink, document);
+              if (err) {
+                   throw new Error('The query was not accepted by the server.');
+                       }
+               context.getResponse().setBody("Success");
+        
+                 }
+                 );
+}
